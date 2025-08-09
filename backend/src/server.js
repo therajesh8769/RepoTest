396c7ab76cc1274b4720ee373554dc80eeb7aaf3
Rajesh <therajesh8769@gmail.com>
2025-08-09 13:54:11 +0530

    initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ const logger = require('./utils/logger');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   logger.info(`Server running on port ${PORT}`);
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
+module.exports = app;
