4d508dcbf054bf1d249f995c6b431a56523bd679
Rajesh <therajesh8769@gmail.com>
2025-08-09 14:00:55 +0530

    deployed frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,13 +10,14 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://repotest-iota.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   
 };
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
